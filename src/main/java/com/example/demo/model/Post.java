770c52defdd96416cd388f0a9b833b97760b7cfe
Artem Solovyov <artemsolovev121@gmail.com>
2025-05-13 13:13:53 +0500

    add bootstrap

--- src/main/java/com/example/demo/model/Post.java
+++ src/main/java/com/example/demo/model/Post.java
@@ -1,18 +1,27 @@
 package com.example.demo.model;
 
+import java.util.Date;
+
 public class Post {
     private String text;
     private Integer likes;
+    private Date creationDate;
 
-    public Post(String text){
+    public Post(String text, Date creationDate) {
         this.text = text;
+        this.creationDate = creationDate;
+        this.likes = 0;
     }
 
     public String getText() {
         return text;
     }
 
-    public void setText(String text) {
-        this.text = text;
+    public Integer getLikes() {
+        return likes;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
     }
 }
